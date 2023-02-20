b73a1d1624ed76fb20d75ba133d47ae6366b664a
bryancol27 <bryan.2709cas@gmail.com>
2023-02-20 11:41:24 -0500

    ADD: FIRST COMMIT WITH CONFIG

---
+++ pages/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// Import components
+import { Navbar } from '@components/navbar';
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar/>
+            at Home
+        </div>
+    );
+};
+
+export default Home;
